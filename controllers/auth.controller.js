0f496b14278a3c8df60fab432099171522222ecf
Eddy <pkw5282@naver.com>
2023-02-02 20:28:29 +0900

    2차

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,68 @@
+const User = require("../models/user.model");
+const authUtil = require("../util/authentication");
+
 function getSignup(req, res, next) {
   res.render("customer/auth/signup");
 }
 
-function getLogin(req, res) {}
+async function signup(req, res, next) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+  try {
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.redirect("/login");
+}
+
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
+
+async function login(req, res, next) {
+  const user = new User(req.body.email, req.body.password);
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  if (!existingUser) {
+    res.redirect("/login");
+    return;
+  }
+
+  const passwordIsCorrect = await user.hasMathchingPassword(
+    existingUser.password
+  );
+  if (!passwordIsCorrect) {
+    res.redirect("/login");
+    return;
+  }
+
+  authUtil.createUserSession(req, existingUser, function () {
+    res.redirect("/");
+  });
+}
+
+function logout(req, res) {
+  authUtil.destroyUserAuthSession(req);
+  res.redirect("/login");
+}
 
 module.exports = {
   getSignup,
   getLogin,
+  signup,
+  login,
+  logout,
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.get("/signup", authController.getSignup);
+router.post('/signup',authController.signup);
 
 router.get("/login", authController.getLogin);
 
+router.post('/login',authController.login);
+
+router.post('/logout',authController.logout);
 
 module.exports =router;
