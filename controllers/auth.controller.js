b6a6e0afd64d091407e4542a97688a85ca4d3a10
Eddy <pkw5282@naver.com>
2023-02-03 16:14:49 +0900

    559강까지 봄

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,61 @@
 const User = require("../models/user.model");
 const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
+const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res, next) {
-  res.render("customer/auth/signup");
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      confirmEmail: "",
+      password: "",
+      fullname: "",
+      street: "",
+      postal: "",
+      city: "",
+    };
+  }
+
+  res.render("customer/auth/signup", { inputData: sessionData });
 }
 
 async function signup(req, res, next) {
+  const enteredData = {
+    email: req.body.email,
+    confirmEmail: req.body["confirm-email"],
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          "Please check your input. Password must be at least 6 characters long, postal code must be 5 characters long",
+        ...enteredData,
+      },
+      function () {
+        res.redirect("/signup");
+      }
+    );
+    return;
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -14,7 +64,22 @@ async function signup(req, res, next) {
     req.body.postal,
     req.body.city
   );
+
   try {
+    const existsAlready = await user.existsAlready();
+    if (existsAlready) {
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "이미 같은 아이디가 있습니다.",
+          ...enteredData,
+        },
+        function () {
+          res.redirect("/signup");
+        }
+      );
+      return;
+    }
     await user.signup();
   } catch (error) {
     next(error);
@@ -24,7 +89,14 @@ async function signup(req, res, next) {
 }
 
 function getLogin(req, res) {
-  res.render("customer/auth/login");
+  let sessionData = sessionFlash.getSessionData(req);
+  if (!sessionData) {
+    sessionData = {
+      email: "",
+      password: "",
+    };
+  }
+  res.render("customer/auth/login", { inputData: sessionData });
 }
 
 async function login(req, res, next) {
@@ -36,8 +108,17 @@ async function login(req, res, next) {
     next(error);
     return;
   }
+
+  const sessionErrorData = {
+    errorMessage:
+      "Invalid credentials - please double-check your email and password!",
+    email: user.email,
+    password: user.password,
+  };
   if (!existingUser) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
@@ -45,7 +126,9 @@ async function login(req, res, next) {
     existingUser.password
   );
   if (!passwordIsCorrect) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
